89f2cdb1b2729251eb0b0b301eef0e017410fd1d
sjain <sapan.jain@panerabread.com>
2018-09-21 09:53:58 -0500

    update enable kafka for consumer

--- src/main/java/com/api/jlabs/kafka/services/KafkaConsumer.java
+++ src/main/java/com/api/jlabs/kafka/services/KafkaConsumer.java
@@ -3,6 +3,7 @@ package com.api.jlabs.kafka.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
  */
 
 @Component
+//@EnableKafka
 public class KafkaConsumer {
 
     private final static  Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
@@ -19,7 +21,7 @@ public class KafkaConsumer {
     @Autowired
     MessageStore messageStore;
 
-    @KafkaListener(topics ="${jlabs.kafka.topic}")
+    @KafkaListener(topics ="${jlabs.kafka.topic}",group = "${spring.kafka.consumer.group-id}")
         public void processMessage(String content){
 
         LOGGER.info("received messages : ", content);
